0036fbc6dbec903f5aa86b8aaa35d30c47fe7ea8
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-07 12:58:07 -0400

    Stopping Apples If They Fall Too Far
    deleting apples if they fall past certain point

--- Assets/Apple.cs
+++ Assets/Apple.cs
@@ -2,10 +2,12 @@
 using System.Collections;
 
 public class Apple : MonoBehaviour {
-
+	public static float bottomY = -20f;
 	// Use this for initialization
 	void Start () {
-	
+		if ( transform.position.y < bottomY ) {
+			Destroy( this.gameObject );
+		}
 	}
 	
 	// Update is called once per frame
