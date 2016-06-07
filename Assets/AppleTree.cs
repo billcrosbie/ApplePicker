8d020eae7220e456e677f556222ec00969cf5ca8
Dimitri Van Sertima <dimitri.vansertima@gmail.com>
2016-06-07 05:18:51 -0400

    Changing Direction
    Changing direction when appletree hits boundary

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -24,6 +24,12 @@ public class AppleTree : MonoBehaviour {
 		pos.x += speed * Time.deltaTime;
 		transform.position = pos;
 		// Changing Direction
+
+		if ( pos.x < -leftAndRightEdge ) {
+			speed = Mathf.Abs(speed); // Move right
+		} else if ( pos.x > leftAndRightEdge ) {
+			speed = -Mathf.Abs(speed); // Move left
+		}
 	
 	}
 }
